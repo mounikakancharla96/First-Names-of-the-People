2f90e8c6fd6af9615dce77419ab1d5becba7e570
Kancharla Mounika <kancharlamounika17@gmail.com>
2022-11-04 04:24:17 +0000

    getting firstname

---
+++ names/index.js
@@ -0,0 +1,9 @@
+const peoplesName = require("../country/state/city/index")
+const getFirstNames = require("../utilities/utils/index")
+
+
+const getPeopleInCity = (peoplesNames) => {
+    return getFirstNames(peoplesNames)
+}
+
+module.exports = getPeopleInCity
